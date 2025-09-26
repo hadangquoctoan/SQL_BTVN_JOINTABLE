b52458f3142db915b5777a5e3e0e71cf1e017822
QUỐC TOÀN DEV <toanhadangquoc@gmail.com>
2025-09-26 10:39:15 +0700

    Update BTVNBUOI1.sql

--- BTVNBUOI1.sql
+++ BTVNBUOI1.sql
@@ -5,6 +5,7 @@ select HOTEN, SODT from NHANVIEN
 Select SOHD, NGHD from HOADON where MAKH = 'KH01'
 Select * from SANPHAM where NUOCSX = 'VN' and GIA < 50000
 Select * from HOADON where NGHD >= '2022-01-01' AND NGHD < '2022-12-31'
+Select * from KHACHHANG where DOANHSO > 30000
 Select * from KHACHHANG where HOTEN like 'Nguyen%'
 Select * from HOADON where TRIGIA > 1000000
 Select * from KHACHHANG where YEAR(NGSINH) = 2000
@@ -12,4 +13,5 @@ Select * from NHANVIEN where YEAR(NGVL) = 2020
 Select distinct MAKH from HOADON
 Select * from SANPHAM where GIA >= 20000 and GIA <= 100000
 Select * from KHACHHANG order by DOANHSO DESC
+
 Select * from HOADON where NGHD between dateadd(day, -7,getdate()) and getdate()
